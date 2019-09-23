ae68227e292ae3b26df2b57fae99a25540a2912c
Natalie Behling <nbehling@nd.edu>
2019-09-23 18:06:52 -0400

    Making Buttons Work

--- project-one/project-one-behling-edit/assets/fadein-fadeout.jquery.js
+++ project-one/project-one-behling-edit/assets/fadein-fadeout.jquery.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     //ID or Class name of the button goes into the first section
-    $('#mr-button').click(function () {
+    $('#mrbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".mr-content").removeClass("invisible");
         $(".mrs-content").addClass("invisible");
     });
-    
-     $('#mrs-button').click(function () {
+
+    $('#mrsbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".mr-content").addClass("invisible");
         $(".mrs-content").removeClass("invisible");
     });
-    
-     $('#family-button').click(function () {
+
+    $('#familybutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".mr-content").removeClass("invisible");
         $(".mrs-content").removeClass("invisible");
