bbef84342a1419aff268f3876ef457db19b0659c
Natalie Behling <nbehling@nd.edu>
2019-09-25 11:33:48 -0400

    Changes to animation

--- project-one/project-one-behling-edit/assets/fadein-out.jquery.js
+++ project-one/project-one-behling-edit/assets/fadein-out.jquery.js
@@ -2,29 +2,30 @@ $(document).ready(function () {
     //ID or Class name of the button goes into the first section
     $('#mrbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $(".mr-content").removeClass("invisible");
-        $(".mrs-content").addClass("invisible");
         $("#mrone").addClass("movemr");
         $("#mrtwo").addClass("movemr");
+        $(".mr-content").removeClass("invisible");
+        $(".mrs-content").addClass("invisible");
     });
 
     $('#mrsbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $(".mr-content").addClass("invisible");
-        $(".mrs-content").removeClass("invisible");
         $("#mrsone").addClass("movemrs");
         $("#mrstwo").addClass("movemrs");
+        $(".mr-content").addClass("invisible");
+        $(".mrs-content").removeClass("invisible");
     
     });
 
     $('#familybutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $(".mr-content").removeClass("invisible");
-        $(".mrs-content").removeClass("invisible");
         $("#mrsone").removeClass("movemrs");
         $("#mrstwo").removeClass("movemrs");
         $("#mrone").removeClass("movemr");
         $("#mrtwo").removeClass("movemr");
+        $(".mr-content").removeClass("invisible");
+        $(".mrs-content").removeClass("invisible");
+    
     });
 
 });
