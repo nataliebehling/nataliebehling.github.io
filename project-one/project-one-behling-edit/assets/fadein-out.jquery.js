8ef90b7b16447d807e355ef670c5dbbaff56b757
Natalie Behling <nbehling@nd.edu>
2019-09-25 11:17:31 -0400

    Changrs to animation

--- project-one/project-one-behling-edit/assets/fadein-out.jquery.js
+++ project-one/project-one-behling-edit/assets/fadein-out.jquery.js
@@ -4,18 +4,27 @@ $(document).ready(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".mr-content").removeClass("invisible");
         $(".mrs-content").addClass("invisible");
+        $("#mrone").addClass("movemr");
+        $("#mrtwo").addClass("movemr");
     });
 
     $('#mrsbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".mr-content").addClass("invisible");
         $(".mrs-content").removeClass("invisible");
+        $("#mrsone").addClass("movemrs");
+        $("#mrstwo").addClass("movemrs");
+    
     });
 
     $('#familybutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".mr-content").removeClass("invisible");
         $(".mrs-content").removeClass("invisible");
+        $("#mrsone").removeClass("movemrs");
+        $("#mrstwo").removeClass("movemrs");
+        $("#mrone").removeClass("movemr");
+        $("#mrtwo").removeClass("movemr");
     });
 
 });
