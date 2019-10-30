9791966cbdf3157e86001cc303d57b64d5f6de4c
Natalie Behling <nbehling@nd.edu>
2019-10-30 13:48:14 -0400

    cool stuff

--- project-one/project-one-behling-edit/assets/fadein-out.jquery.js
+++ project-one/project-one-behling-edit/assets/fadein-out.jquery.js
@@ -2,29 +2,29 @@ $(document).ready(function () {
     //ID or Class name of the button goes into the first section
     $('#mrbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#mrone").removeClass("movemr");
-        $("#mrtwo").removeClass("movemr");
-        $(".mr-content").removeClass1("ininvisible");
-        $(".mrs-content").removeClass("ininvisible");
+        $("#mrone").addClass("movemr");
+        $("#mrtwo").addClass("movemr");
+        $(".mr-content").removeClass("invisible");
+        $(".mrs-content").addClass("invisible");
     });
 
     $('#mrsbutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#mrsone").removeClass("movemrs");
-        $("#mrstwo").removeClass("movemrs");
-        $(".mr-content").removeClass("ininvisible");
-        $(".mrs-content").removeClass1("ininvisible");
+        $("#mrsone").addClass("movemrs");
+        $("#mrstwo").addClass("movemrs");
+        $(".mr-content").addClass("invisible");
+        $(".mrs-content").removeClass("invisible");
     
     });
 
     $('#familybutton').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#mrsone").removeClass1("movemrs");
-        $("#mrstwo").removeClass1("movemrs");
-        $("#mrone").removeClass1("movemr");
-        $("#mrtwo").removeClass1("movemr");
-        $(".mr-content").removeClass1("ininvisible");
-        $(".mrs-content").removeClass1("ininvisible");
+        $("#mrsone").removeClass("movemrs");
+        $("#mrstwo").removeClass("movemrs");
+        $("#mrone").removeClass("movemr");
+        $("#mrtwo").removeClass("movemr");
+        $(".mr-content").removeClass("invisible");
+        $(".mrs-content").removeClass("invisible");
     
     });
 
