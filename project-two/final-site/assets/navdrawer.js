db38dd307dd2d025bd0735af7707d6837f28e195
Natalie Behling <nbehling@nd.edu>
2019-11-04 10:26:04 -0500

    Office Hours Stuff

---
+++ project-two/final-site/assets/navdrawer.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //ID or Class name of the button goes into the first section
+    $('.openbtn').click(function () {
+        //ID or Class name of the modal you want to appear son click goes into this second area#
+        $("#mySidepanel").removeClass("drawer"); 
+    });
+    
+    $('.closebtn').click(function () {
+        //ID or Class name of the modal you want to appear son click goes into this second area#
+        $("#mySidepanel").addClass("drawer"); 
+    });
+});
