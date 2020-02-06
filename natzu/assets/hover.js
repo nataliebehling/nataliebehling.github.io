9f129ea12666b6934d4d15b9f6cc4fb15e7adaf7
Natalie Behling <nbehling@nd.edu>
2020-02-05 19:21:33 -0500

    pushing visualization project

---
+++ natzu/assets/hover.js
@@ -0,0 +1,347 @@
+
+    
+    $(document).ready(function() { 
+            $(".p-ivanatrump").hover(function() { 
+                $("#popup-ivanatrump").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-ivanatrump").css("visibility", "hidden"); 
+            }); 
+        
+        $(".p-gail").hover(function() { 
+                $("#popup-gail").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-gail").css("visibility", "hidden"); 
+            }); 
+        
+        $(".p-barbarares").hover(function() { 
+                $("#popup-barbarares").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-barbarares").css("visibility", "hidden"); 
+            }); 
+        
+        $(".p-barbarares2").hover(function() { 
+                $("#popup-barbarares2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-barbarares2").css("visibility", "hidden"); 
+            }); 
+    
+        $(".p-jodieseal").hover(function() { 
+                $("#popup-jodieseal").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-jodieseal").css("visibility", "hidden"); 
+            }); 
+        
+        $(".p-aliciamachado").hover(function() { 
+                $("#popup-aliciamachado").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciamachado").css("visibility", "hidden"); 
+            }); 
+    
+        $(".p-aliciamachado2").hover(function() { 
+                $("#popup-aliciamachado2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciamachado2").css("visibility", "hidden"); 
+            });
+        
+         $(".p-aliciamachado3").hover(function() { 
+                $("#popup-aliciamachado3").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciamachado3").css("visibility", "hidden"); 
+            });
+        
+         $(".p-aliciamachado4").hover(function() { 
+                $("#popup-aliciamachado4").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciamachado4").css("visibility", "hidden"); 
+            });
+        
+         $(".p-aliciamachado5").hover(function() { 
+                $("#popup-aliciamachado5").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciamachado5").css("visibility", "hidden"); 
+            });
+        
+        $(".p-allapprentice").hover(function() { 
+                $("#popup-allapprentice").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-allapprentice").css("visibility", "hidden"); 
+            });
+        
+        $(".p-allapprentice2").hover(function() { 
+                $("#popup-allapprentice2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-allapprentice2").css("visibility", "hidden"); 
+            });
+        
+        $(".p-kristifrank").hover(function() { 
+                $("#popup-kristifrank").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-kristifrank").css("visibility", "hidden"); 
+            });
+        
+        $(".p-rebeccaarndt").hover(function() { 
+                $("#popup-rebeccaarndt").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rebeccaarndt").css("visibility", "hidden"); 
+            });
+        
+        $(".p-rosieodonnll").hover(function() { 
+                $("#popup-rosieodonnll").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll").css("visibility", "hidden"); 
+            });
+        
+        $(".p-rosieodonnll2").hover(function() { 
+                $("#popup-rosieodonnll2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll2").css("visibility", "hidden"); 
+            });
+        
+          $(".p-rosieodonnll3").hover(function() { 
+                $("#popup-rosieodonnll3").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll3").css("visibility", "hidden"); 
+            });
+        
+        $(".p-suekwiatkowski").hover(function() { 
+                $("#popup-suekwiatkowski").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-suekwiatkowski").css("visibility", "hidden"); 
+            });
+        
+           $(".p-ivankatrump").hover(function() { 
+                $("#popup-ivankatrump").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-ivankatrump").css("visibility", "hidden"); 
+            });
+        
+        $(".p-gloriaallred").hover(function() { 
+                $("#popup-gloriaallred").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-gloriaallred").css("visibility", "hidden"); 
+            });
+
+         $(".p-rosieodonnll4").hover(function() { 
+                $("#popup-rosieodonnll4").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll4").css("visibility", "hidden"); 
+            });
+        
+        $(".p-allmissuniverse").hover(function() { 
+                $("#popup-allmissuniverse").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-allmissuniverse").css("visibility", "hidden"); 
+            });
+        
+         $(".p-elizabethbeck").hover(function() { 
+                $("#popup-elizabethbeck").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-elizabethbeck").css("visibility", "hidden"); 
+            });
+        
+         $(".p-rosieodonnll5").hover(function() { 
+                $("#popup-rosieodonnll5").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll5").css("visibility", "hidden"); 
+            });
+        
+        $(".p-branderoderick").hover(function() { 
+                $("#popup-branderoderick").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-branderoderick").css("visibility", "hidden"); 
+            });
+        
+        $(".p-arianahuffington").hover(function() { 
+                $("#popup-arianahuffington").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-arianahuffington").css("visibility", "hidden"); 
+            });
+        
+        $(".p-arianahuffington2").hover(function() { 
+                $("#popup-arianahuffington2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-arianahuffington2").css("visibility", "hidden"); 
+            });
+        
+         $(".p-arianahuffington3").hover(function() { 
+                $("#popup-arianahuffington3").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-arianahuffington3").css("visibility", "hidden"); 
+            });
+        
+         $(".p-kristenstewartmsuniverse").hover(function() { 
+                $("#popup-kristenstewartmsuniverse").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-kristenstewartmsuniverse").css("visibility", "hidden"); 
+            });
+        
+         $(".p-gailcollins").hover(function() { 
+                $("#popup-gailcollins").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-gailcollins").css("visibility", "hidden"); 
+            });
+        
+        $(".p-rosieodonnll7").hover(function() { 
+                $("#popup-rosieodonnll7").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll7").css("visibility", "hidden"); 
+            });
+        
+         $(".p-rosieodonnll6").hover(function() { 
+                $("#popup-rosieodonnll6").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-rosieodonnll6").css("visibility", "hidden"); 
+            });
+        
+         $(".p-carlyfiorina").hover(function() { 
+                $("#popup-carlyfiorina").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-carlyfiorina").css("visibility", "hidden"); 
+            });
+        
+        $(".p-carlyfiorina2").hover(function() { 
+                $("#popup-carlyfiorina2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-carlyfiorina2").css("visibility", "hidden"); 
+            });
+        
+        $(".p-arianahuffington4").hover(function() { 
+                $("#popup-arianahuffington4").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-arianahuffington4").css("visibility", "hidden"); 
+            });
+        
+         $(".p-arianahuffington5").hover(function() { 
+                $("#popup-arianahuffington5").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-arianahuffington5").css("visibility", "hidden"); 
+            });
+        
+         $(".p-megynkelly").hover(function() { 
+                $("#popup-megynkelly").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-megynkelly").css("visibility", "hidden"); 
+            });
+        
+        $(".p-hillary").hover(function() { 
+                $("#popup-hillary").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-hillary").css("visibility", "hidden"); 
+            });
+        
+        $(".p-hillaryclinton").hover(function() { 
+                $("#popup-hillaryclinton").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-hillaryclinton").css("visibility", "hidden"); 
+            });
+        
+        $(".p-hillaryclinton2").hover(function() { 
+                $("#popup-hillaryclinton2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-hillaryclinton2").css("visibility", "hidden"); 
+            });
+        
+         $(".p-hillaryclinton3").hover(function() { 
+                $("#popup-hillaryclinton3").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-hillaryclinton3").css("visibility", "hidden"); 
+            });
+        
+        $(".p-hillaryclinton4").hover(function() { 
+                $("#popup-hillaryclinton4").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-hillaryclinton4").css("visibility", "hidden"); 
+            });
+        
+        $(".p-hillaryclinton5").hover(function() { 
+                $("#popup-hillaryclinton5").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-hillaryclinton5").css("visibility", "hidden"); 
+            });
+        
+        $(".p-megynkelly2").hover(function() { 
+                $("#popup-megynkelly2").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-megynkelly2").css("visibility", "hidden"); 
+            });
+        
+        $(".p-megynkelly3").hover(function() { 
+                $("#popup-megynkelly3").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-megynkelly3").css("visibility", "hidden"); 
+            });
+        
+        $(".p-aliciamachado6").hover(function() { 
+                $("#popup-aliciamachado6").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciamachado6").css("visibility", "hidden"); 
+            });
+        
+        $(".p-aliciahillary").hover(function() { 
+                $("#popup-aliciahillary").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-aliciahillary").css("visibility", "hidden"); 
+            });
+        
+        $(".p-merylhillary").hover(function() { 
+                $("#popup-merylhillary").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-merylhillary").css("visibility", "hidden"); 
+            });
+        
+         $(".p-mika").hover(function() { 
+                $("#popup-mika").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-mika").css("visibility", "hidden"); 
+            });
+        
+         $(".p-gillibrand").hover(function() { 
+                $("#popup-gillibrand").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-gillibrand").css("visibility", "hidden"); 
+            });
+        
+        $(".p-macron").hover(function() { 
+                $("#popup-macron").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-macron").css("visibility", "hidden"); 
+            });
+        
+        $(".p-stormydaniels").hover(function() { 
+                $("#popup-stormydaniels").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-stormydaniels").css("visibility", "hidden"); 
+            });
+        
+         $(".p-waters").hover(function() { 
+                $("#popup-waters").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-waters").css("visibility", "hidden"); 
+            });
+        
+        $(".p-AOC").hover(function() { 
+                $("#popup-AOC").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-AOC").css("visibility", "hidden"); 
+            });
+        
+        $(".p-pelosi").hover(function() { 
+                $("#popup-pelosi").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-pelosi").css("visibility", "hidden"); 
+            });
+        
+        $(".p-thurn").hover(function() { 
+                $("#popup-thurn").css("visibility", "visible"); 
+            }, function() { 
+                $("#popup-thurn").css("visibility", "hidden"); 
+            });
+
+
+
+
+
+
+
+
+});
