149accf93cba6eda44e62bd9ab038bfb44cbc2d6
Natalie Behling <nbehling@nd.edu>
2020-04-30 22:25:59 -0400

    adding maps 2

--- COVIDQ/final/assets/fadein-out.jquery.js
+++ COVIDQ/final/assets/fadein-out.jquery.js
@@ -1,46 +1,62 @@
 $(document).ready(function () {
     //ID or Class name of the button goes into the first section
-    
-    
-    
-    
-     $('#white').click(function () {
+
+
+
+
+    $('#asian').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#chicago-black-pop").addClass("invisible");
+        $("#chicago-covid19").addClass("invisible");
+        $("#chicago-asian-pop").removeClass("invisible");
+        $("#chicago-hispanic-pop").addClass("invisible");
+        $("#chicago-white-pop").addClass("invisible");
+
+    });
+
+
+
+    $('#white').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#chicago-black-pop").addClass("invisible");
         $("#chicago-covid19").addClass("invisible");
         $("#chicago-white-pop").removeClass("invisible");
-         $("#chicago-hispanic-pop").addClass("invisible");  
-         
+        $("#chicago-hispanic-pop").addClass("invisible");
+        $("#chicago-asian-pop").addClass("invisible");
+
     });
-    
-    
+
+
     $('#hispanic').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#chicago-black-pop").addClass("invisible");
         $("#chicago-covid19").addClass("invisible");
-             $("#chicago-hispanic-pop").removeClass("invisible");
+        $("#chicago-hispanic-pop").removeClass("invisible");
         $("#chicago-white-pop").addClass("invisible");
-         
+        $("#chicago-asian-pop").addClass("invisible");
+
     });
-    
-    
+
+
     $('#black').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#chicago-black-pop").removeClass("invisible");
         $("#chicago-covid19").addClass("invisible");
         $("#chicago-hispanic-pop").addClass("invisible");
         $("#chicago-white-pop").addClass("invisible");
-         
+        $("#chicago-asian-pop").addClass("invisible");
+
     });
-    
+
     $('#covid19').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#chicago-black-pop").addClass("invisible");
         $("#chicago-covid19").removeClass("invisible");
         $("#chicago-hispanic-pop").addClass("invisible");
         $("#chicago-white-pop").addClass("invisible");
-         
+        $("#chicago-asian-pop").addClass("invisible");
+
     });
 
-   
+
 });
